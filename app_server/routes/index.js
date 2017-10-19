33260a0b0509b9805db96536983a64be314e6300
Mark Thew <Mark.Thew@consolidated.com>
2017-10-19 11:52:44 -0700

    section 6.2 (corrected)

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,20 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var ctrlLocations = require('../controllers/locations');
-var ctrlReviews = require('../controllers/reviews');
+var ctrlOthers = require('../controllers/others');
 
-/* Locations */
-router.get('/locations', ctrlLocations.locationsListByDistance);
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
 
-router.post('/locations', ctrlLocations.locationsCreate);
-router.get('/locations/:locationid', ctrlLocations.locationsReadOne);
-router.put('/locations/:locationid', ctrlLocations.locationsUpdateOne);
-router.delete('/locations/:locationid', ctrlLocations.locationsDeleteOne);
-
-/* Reviews */
-router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
-router.get('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsReadOne);
-router.put('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsUpdateOne);
-router.delete('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviews.DeleteOne);
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
